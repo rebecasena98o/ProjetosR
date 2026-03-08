dc4a0e8c1a1d20c66d620490b03de7d3580c0512
Rebeca Sena <r3b3casena@gmail.com>
2026-03-07 22:27:16 -0300

    Feat: Medidas de dispersão adicionadas para a variável Idade das Vítimas

--- Comandos_Trabalho.R
+++ Comandos_Trabalho.R
@@ -51,19 +51,28 @@ tabW
 #Freq. relativa - arredonda para 2 casas
 round(prop.table(table(CVLI_2024_a_2025$`Dia da Semana`)) * 100, 2)
 
+
+# ----------------------------------------------------------------
+
+
 #Determinação das medidas de posição, dispersão e separação 
 
-idade <- CVLI_2024_a_2025$`Idade da Vítima`
+# IDADE DA VÍTIMA 
 
 #Medidas de posição - onde os dados se concentram
 
-#Média
-MEdia <- mean(CVLI_2024_a_2025$`Idade da Vítima`)
-MEdia
+#idade_numerica armazena todas as idades como numero e "não informado" vira NA
+idade_numerica <- as.numeric(CVLI_2024_a_2025$`Idade da Vítima`)
+
+#idade_valida armazena idades ignorando todos NA
+idade_valida <- idade_numerica[!is.na(idade_numerica)]
+
+#idade armazena media de idade das vitimas
+idadeMedia <- mean(idade_valida)
+idadeMedia
 
 #Mediana
-median(idade, na.rm = TRUE)
-
+median(idadeMedia, na.rm = TRUE)
 
 #Moda
 moda <- function(x) {
@@ -74,4 +83,64 @@ moda <- function(x) {
   ux[which.max(tabulate(match(x, ux)))]  # valor mais frequente
   
 }
-moda(idade)
+
+moda(idadeMedia)
+
+# MEDIDAS DE DISPERSÃO
+#IDADE VÍTIMA
+#Amplitude
+
+#amplitude_max <- as.numeric(max(CVLI_2024_a_2025$`Idade da Vítima`, na.rm = TRUE))
+#amplitude_min <- as.numeric(min(CVLI_2024_a_2025$`Idade da Vítima`, na.rm = TRUE))
+#amplitude_max_valida <- amplitude_max[!is.na(amplitude_max)]
+#amplitude_min_valida <- amplitude_min[!is.na(amplitude_min)]
+#amplitude_direta <- amplitude_max_valida - amplitude_min_valida
+#amplitude_direta
+
+#código limpo e de forma direta
+idadesV <- as.numeric(CVLI_2024_a_2025$`Idade da Vítima`)
+summary(idadesV) # verifica se o resultado está correto antes de rodar
+amplitude_direta <- max(idadesV, na.rm = TRUE) - min(idadesV, na.rm = TRUE)
+amplitude_direta
+
+#Variância 
+
+varianca <- as.numeric(CVLI_2024_a_2025$`Idade da Vítima`) #variância armazena 
+#todas as idades como número e "não informado" vira NA
+
+varianca_valida <- varianca[!is.na(varianca)] # armazena idades ignorando todos os NA
+
+variancaOne <- var(varianca_valida) #armazena media de idade das vitimas
+variancaOne
+
+#Desvio Padrão
+
+Desvio <- as.numeric(CVLI_2024_a_2025$`Idade da Vítima`) #Desvio armazena
+#todas as idades como número e "não informado" vira NA
+
+Desvio_valido <- Desvio[!is.na(Desvio)] # Armazena idades ignorando todos os NA
+
+DesvioPadrao <- sd(Desvio_valido) #Armazena desvio padrão das idades das vítimas
+DesvioPadrao # resultado
+
+#coeficiente de variação
+cv <- (DesvioPadrao/idadeMedia) * 100 
+cv
+
+#separatrizes - valores
+#que dividem um conjunto de dados 
+#(ordenado do menor para o maior) em partes iguais
+
+# Quartis (Q): Dividem os dados em 4 partes (25% cada)
+OneQ <- as.numeric(CVLI_2024_a_2025$`Idade da Vítima`)
+OneQ_valido <- OneQ[!is.na(OneQ)]
+Quartis <- quantile(OneQ_valido)
+Quartis
+
+#Bloxplot - é um gráfico usado para visualizar a distribuição e a 
+#dispersão dos seus dados, destacando as separatrizes que você acabou de calcular
+boxplot(idade_valida, 
+        main = "Distribuição de Idade das Vítimas",
+        ylab = "Idade",
+        col = "lightblue",
+        border = "darkblue")
